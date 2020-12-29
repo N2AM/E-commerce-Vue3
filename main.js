a7c1de8afdd9cf8fb7b725d347f0e8fbb2afac0f
AL REFAI Nouran OBS/CMI <sqwq3383@orange.com>
2020-12-29 12:50:25 +0100

    attr binding

---
+++ main.js
@@ -0,0 +1,10 @@
+// Create Vue instance
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Sock',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://nouranalrefai.com'
+        }
+    }
+})
